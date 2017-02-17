2667e44dc52b0d2f21a4a04b308871b9cfeebb73
bcalain <bcalain@gmail.com>
2017-02-17 23:47:36 +0000

    Add proceso SKMAdmin

---
+++ esquelemod/e_modulos/procesos/SKMAdmin/InicioConfig.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+$path2load = 'esquelemod/e_modulos/procesos/' . $this->EEoNucleo->pathDirRaizProcesoEjecucion() . '/';
+$path2loadAbs = $this->EEoNucleo->pathAbsolutoDirRaizProcesoEjecucion() . '/';
+$path2loadEsquelemod = $this->EEoNucleo->pathDirEsquelemod() . '/';
+
+require $path2loadAbs.'root/ChequeoSesion.php';
+require_once $path2loadEsquelemod . 'e_modulos/nucleo/nucleo_bib_funciones/herramientas/spyc.php';
+
+require_once $path2loadAbs . 'class/class_PathBootstrap.php';
+
+// Control del tab seleccionado
+$arrayTab[] = array();
+if (isset($_GET['tab'])) {
+    $selectTab = filter_input(INPUT_GET, 'tab', FILTER_SANITIZE_SPECIAL_CHARS);
+    $arrayTab[$selectTab] = 'class="active"';
+}
+
+// Leemos config del sistema
+$datos = \Emod\Nucleo\Herramientas\Spyc::YAMLLoad($path2loadEsquelemod .'e_sistema/e_sistema_config.cnf', false);
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Configuracion</title>
+        <link rel="stylesheet" href="<?php echo $path2load ?>style.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo $pathRelBootstrap ?>/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo $pathRelBootstrap ?>/bootstrap/dist/css/bootstrap-theme.min.css">
+        <!-- <script src="<?php echo $path2load ?>js-bootstrap/bootstrap.min.jsm"></script>         -->
+    </head>
+    <body>
+        <script src="<?php echo $pathRelBootstrap ?>/bootstrap/assets/js/jquery.js"></script>
+        <script src="<?php echo $pathRelBootstrap ?>/bootstrap/dist/js/bootstrap.min.js"></script>
+        <div >
+            <h3>SKM. <small>Administraci&oacute;n del sistema</small></h3>
+        </div>
+
+        <nav class="navbar navbar-default" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <!-- Sin uso por el momento 
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                
+                <a class="navbar-brand" href="#">Inicio</a>
+                -->
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li <?php echo $arrayTab[0] ?>><a href="?app=inicioconfig&tab=0">Inicio</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Configuraci&oacute;n principal <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="?app=inicioconfig&tab=1&fs=sistema">Valores del sistema</a></li>
+                            <li><a href="?app=inicioconfig&tab=1&fs=herramientas">Valores referentes a Herramientas</a></li>
+                            <li><a href="#">Valores referentes a Utiles</a></li>
+                            <li><a href="#">Tratamiento de errores</a></li>
+                            <li><hr></li>
+                            <li><a href="#">Valores referentes a bloques de procesos</a></li>
+
+                        </ul>
+                    </li>        
+                    <li <?php echo $arrayTab[2] ?>><a href="?app=inicioconfig&tab=2">Usuarios</a></li>
+                    <li <?php echo $arrayTab[3] ?>><a href="?app=inicioconfig&tab=3">Acerca de</a></li>
+
+                </ul>
+                <!-- La busqueda no estará implementada por ahora 
+                <form class="navbar-form navbar-left" role="search">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                  </div>
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </form> 
+                -->
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="?app=config&log=out" class="navbar-link">Cerrar sesi&oacute;n</a></li>
+                </ul>
+
+            </div><!-- /.navbar-collapse -->
+        </nav>
+
+        <?php
+        if (isset($_GET['tab'])) {
+            switch ($selectTab) {
+                case 0:
+                    require $path2loadAbs . 'root/inicio.php';
+                    break;
+                case 1:
+                    require $path2loadAbs . 'root/MainConfig.php';
+                    break;
+                case 2:
+                    //require $path2loadAbs . 'root/MainConfig.php';
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        ?>
+
+</html>
---
+++ esquelemod/e_modulos/procesos/SKMAdmin/LoginConfig.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<?php
+/*
+  SKM Config Herramienta para la configuración de SOCKELETOM (SKM)
+  Copyright (C) 2016  Oscar Luis Inguanzo Martínez
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  email-contact oscarlim@nauta.cu
+ */
+/**
+ * SKM Config
+ * @version 0.0.1
+ * @author Oscar Luis Inguanzo Martínez <oscarlim@nauta.cu>
+ * @link https://github.com/
+ * @copyright Copyright 2016
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL License
+ */
+$path2load = 'esquelemod/e_modulos/procesos/' . $this->EEoNucleo->pathDirRaizProcesoEjecucion() . '/';
+$path2loadAbs = $this->EEoNucleo->pathAbsolutoDirRaizProcesoEjecucion() . '/';
+$path2loadEsquelemod = $this->EEoNucleo->pathDirEsquelemod() . '/';
+
+
+if (isset($_GET['log']) && $_GET['log'] == 'out') {
+    if (!@include_once $path2loadAbs . 'class/class_SessionControl.php') {
+        die("Error. Imposible cargar aplicaci&oacute;n. Existe un problema con el chequeo de la sesi&oacute;n. " . basename(__FILE__) . '. Linea:' . __LINE__);
+    } else {
+        session_start();
+        $varSession = new class_SessionControl($path2load);
+        $varSession->deleteTmpSession();
+        $_SESSION = array();
+        session_destroy();
+    }
+}
+
+require_once $path2loadEsquelemod . 'e_modulos/nucleo/nucleo_bib_funciones/herramientas/spyc.php';
+require_once $path2loadAbs . 'class/class_PathBootstrap.php';
+
+$datosConfig = \Emod\Nucleo\Herramientas\Spyc::YAMLLoad($path2loadAbs . 'config.yml', false);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SKM Administracion</title>
+        <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="<?php echo $path2load ?>style.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo $pathRelBootstrap ?>/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo $pathRelBootstrap ?>/bootstrap/dist/css/bootstrap-theme.min.css">
+        <!-- <script src="<?php echo $path2load ?>js-bootstrap/bootstrap.min.js"></script> -->       
+    </head>
+    <body>
+        <table border="0" align="center">
+            <tr><td>
+                    <div class="contenedor">
+                        <h3 class="form-signin-heading" align="center">SKM Admin</h3>
+                        <form class="form-signin" action="?app=chklogin" method="post">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <p><input name="inputUser" type="text" class="form-control" placeholder="Usuario" autofocus></p>
+                                    <p><input name="inputPass" type="password" class="form-control" placeholder="Contraseña"></p>
+                                </div>
+                                <div class="panel-footer" align="right"><button class="btn btn-primary btn-sm" type="submit">Aceptar</button></div>
+                            </div>
+                        </form>
+                    </div>
+                    <?php if (isset($_GET['login']) && $_GET['login'] === 'false') { ?>
+                        <div class="alert alert-danger">Usuario o contrase&ntilde;a incorrectos</div>
+                    <?php } ?>
+                </td>
+            </tr>
+            <tr><td align="center"><h5>ver <?php echo $datosConfig['version'] ?></h5></td></tr>
+        </table>
+
+    </body>
+</html>
